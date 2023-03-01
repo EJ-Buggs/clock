fd51b1b411a57805ba41e311078363af0b481028
ebuggs <ebuggs212@gmail.com>
2023-03-01 05:43:11 -0500

    add background, fix hour hand, add transparency

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ function time() {
   const h = new Date().getHours();
   const m = new Date().getMinutes();
   const s = new Date().getSeconds();
-  hour.style.transform = `rotate(${h * 15 + 90}deg)`;
+  hour.style.transform = `rotate(${h * 30 + 90}deg)`;
   minute.style.transform = `rotate(${m * 6 + 90}deg)`;
   second.style.transform = `rotate(${s * 6 + 90}deg)`;
+  console.log(h);
 }
 
 setInterval(time, 1000);
