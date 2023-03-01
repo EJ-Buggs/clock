56ac3748bcfe1e455a1a08f623ce86060d610e5a
ebuggs <ebuggs212@gmail.com>
2023-03-01 03:43:00 -0500

    add vanilla analog clock

---
+++ script.js
@@ -0,0 +1,14 @@
+const second = document.querySelector(".second");
+const minute = document.querySelector(".minute");
+const hour = document.querySelector(".hour");
+
+function time() {
+  const h = new Date().getHours();
+  const m = new Date().getMinutes();
+  const s = new Date().getSeconds();
+  hour.style.transform = `rotate(${h * 15 + 90}deg)`;
+  minute.style.transform = `rotate(${m * 6 + 90}deg)`;
+  second.style.transform = `rotate(${s * 6 + 90}deg)`;
+}
+
+setInterval(time, 1000);
