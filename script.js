76ad01aa6cc05b3ec376ce89fb886c5e7ab2cab5
ebuggs <ebuggs212@gmail.com>
2023-03-07 01:56:54 -0500

    add fonts, finish css

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const second = document.querySelector(".second");
-const minute = document.querySelector(".minute");
-const hour = document.querySelector(".hour");
+const second = document.querySelector("#ss");
+const minute = document.querySelector("#mm");
+const hour = document.querySelector("#hh");
 
-function time() {
-  const h = new Date().getHours() * 30 + 90;
-  const m = new Date().getMinutes() * 6 + 90;
-  const s = new Date().getSeconds() * 6 + 90;
-  hour.style.transform = `rotate(${h}deg)`;
+setInterval(() => {
+  const h = new Date().getHours() * 30;
+  const m = new Date().getMinutes() * 6;
+  const s = new Date().getSeconds() * 6;
+  hour.style.transform = `rotate(${h + m / 12}deg)`;
   minute.style.transform = `rotate(${m}deg)`;
   second.style.transform = `rotate(${s}deg)`;
-}
+  console.log(h);
+});
 
-setInterval(time, 1000);
+// function time() {
+//   const h = new Date().getHours() * 30;
+//   const m = new Date().getMinutes() * 6;
+//   const s = new Date().getSeconds() * 6;
+//   hour.style.transform = `rotate(${h}deg)`;
+//   minute.style.transform = `rotate(${m}deg)`;
+//   second.style.transform = `rotate(${s}deg)`;
+//   console.log(h);
+// }
+
+// setInterval(time, 1000);
